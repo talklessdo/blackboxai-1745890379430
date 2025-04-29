640ace9d4793d37c5cd9bd5c6e5095eeeb2280e9
M. Isa Daud <77084990+talklessdo@users.noreply.github.com>
2025-04-29 08:59:22 +0700

    Add Bengkel/database/migrations/2025_04_29_014129_create_vehicles_table.php 1745891962437

---
+++ Bengkel/database/migrations/2025_04_29_014129_create_vehicles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->string('license_plate');
+            $table->string('brand');
+            $table->string('model');
+            $table->integer('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
